12fc670c6a76299e719f8487e077bcd109b8159a
rorellana-b <ronalbalmoreorellanabonilla@MacBook-Air-de-Ronal.local>
2025-09-28 10:10:02 -0600

    activities 2 errors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
@@ -11,10 +11,11 @@ const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
-
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities");
 
 /* ***********************
- * View Engine and Templates 
+ * View Engine and Templates
  *************************/
 app.set("view engine", "ejs");
 app.use(expressLayouts);
@@ -23,15 +24,48 @@ app.set("layout", "./layouts/layout"); // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);
 //index route
 app.get("/", baseController.buildHome);
 
 // Index Routes
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Home"
-  })
+    title: "Home",
+  });
+});
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome));
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+
+  let message;
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
 });
 
 /* ***********************
@@ -45,5 +79,5 @@ const host = process.env.HOST;
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on ${host}:${port}`);
 });
